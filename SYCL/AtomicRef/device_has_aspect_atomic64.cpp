9bd460bab11bbd150b662513cb75cc6664a81ff6
Denis Kabanov <denis.kabanov@intel.com>
2022-03-25 19:55:22 +0300

    Use backend macros

--- SYCL/AtomicRef/device_has_aspect_atomic64.cpp
+++ SYCL/AtomicRef/device_has_aspect_atomic64.cpp
@@ -1,14 +1,14 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out %opencl_lib
-// %level_zero_options RUN: %CPU_RUN_PLACEHOLDER %t.out RUN:
-// %GPU_RUN_PLACEHOLDER %t.out RUN: %ACC_RUN_PLACEHOLDER %t.out
-
-// REQUIRES: (opencl && opencl_icd) || (level_zero && level_zero_dev_kit) || hip
+// REQUIRES: opencl_icd && level_zero_dev_kit
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out %opencl_lib %level_zero_options
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 // XFAIL: hip
 // Expected failure because hip does not have atomic64 check implementation
 
-#include <CL/cl.h>
 #include <CL/sycl.hpp>
+#include <CL/cl.h>
 #include <level_zero/ze_api.h>
 
 using namespace sycl;
@@ -17,8 +17,7 @@ int main() {
   queue Queue;
   device Dev = Queue.get_device();
   bool Result;
-  switch (Dev.get_backend()) {
-  case backend::opencl: {
+  #ifdef SYCL_BACKEND_OPENCL
     // Get size for string of extensions
     size_t ExtSize;
     clGetDeviceInfo(get_native<backend::opencl>(Dev), CL_DEVICE_EXTENSIONS, 0,
@@ -38,9 +37,9 @@ int main() {
     assert(Dev.has(aspect::atomic64) == Result &&
            "The Result value differs from the implemented atomic64 check on "
            "the OpenCL backend.");
-    break;
-  }
-  case backend::ext_oneapi_level_zero: {
+    return 0;
+#endif // SYCL_BACKEND_OPENCL
+#ifdef SYCL_EXT_ONEAPI_BACKEND_LEVEL_ZERO
     ze_device_module_properties_t Properties;
     zeDeviceGetModuleProperties(get_native<backend::ext_oneapi_level_zero>(Dev),
                                 &Properties);
@@ -51,13 +50,11 @@ int main() {
     assert(Dev.has(aspect::atomic64) == Result &&
            "The Result value differs from the implemented atomic64 check on "
            "the L0 backend.");
-  }
-  case backend::ext_oneapi_hip: {
+    return 0;
+#endif // SYCL_EXT_ONEAPI_BACKEND_LEVEL_ZERO
+#if defined(SYCL_EXT_ONEAPI_BACKEND_CUDA) || defined(SYCL_EXT_ONEAPI_BACKEND_HIP)
     Dev.has(aspect::atomic64);
-    break;
-  }
-  default:
-    assert(false && "Used unexpected backend.");
-  }
-  return 0;
+    return 0;
+#endif // SYCL_EXT_ONEAPI_BACKEND_CUDA || SYCL_EXT_ONEAPI_BACKEND_HIP
+  return -1;
 }
