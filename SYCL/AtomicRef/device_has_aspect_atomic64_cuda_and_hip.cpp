1f07debcebd72c93642820c35008b6c4a7002f8d
Denis Kabanov <denis.kabanov@intel.com>
2022-03-31 18:19:00 +0300

    Split test cpp file

---
+++ SYCL/AtomicRef/device_has_aspect_atomic64_cuda_and_hip.cpp
@@ -0,0 +1,15 @@
+// REQUIRES: cuda || hip
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+// XFAIL: hip
+// Expected failure because hip does not have atomic64 check implementation
+
+#include <CL/sycl.hpp>
+
+using namespace sycl;
+
+int main() {
+  Dev.has(aspect::atomic64);
+  return 0;
+}
